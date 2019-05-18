5ede0102b1fe7233e3681ba32a37ac5bb6230892
Chris Rebbelin <s0548921@htw-berlin.de>
2019-05-18 17:53:47 +0200

    moved token to env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  MAPBOX_API_TOKEN: 'pk.eyJ1IjoibWFwcGluZ2xlaG5lIiwiYSI6ImNqdmxhZzMyYjEwNXAzeWw2ZTZqcG5waWUifQ.PtU676kvusvROvh_N34FxA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  MAPBOX_API_TOKEN: 'pk.eyJ1IjoibWFwcGluZ2xlaG5lIiwiYSI6ImNqdmxhZzMyYjEwNXAzeWw2ZTZqcG5waWUifQ.PtU676kvusvROvh_N34FxA'
 };
 
 /*

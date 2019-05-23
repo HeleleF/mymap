ac720e31c006d28c7c5ce09337af02cdd28d5642
Chris Rebbelin <s0548921@htw-berlin.de>
2019-05-23 20:16:15 +0200

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  MAPBOX_API_TOKEN: 'pk.eyJ1IjoibWFwcGluZ2xlaG5lIiwiYSI6ImNqdmxhZzMyYjEwNXAzeWw2ZTZqcG5waWUifQ.PtU676kvusvROvh_N34FxA'
+  MAPBOX_API_TOKEN: 'pk.eyJ1IjoibWFwcGluZ2xlaG5lIiwiYSI6ImNqdmxhZzMyYjEwNXAzeWw2ZTZqcG5waWUifQ.PtU676kvusvROvh_N34FxA',
+  firebaseConfig: {
+    apiKey: "AIzaSyA1abJCUsKrI58kUPALI3Oz3nLJIgty4ZQ",
+    authDomain: "meine-pwa.firebaseapp.com",
+    databaseURL: "https://meine-pwa.firebaseio.com",
+    projectId: "meine-pwa",
+    storageBucket: "meine-pwa.appspot.com",
+    messagingSenderId: "340118471403",
+    appId: "1:340118471403:web:4e13847887689032"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  MAPBOX_API_TOKEN: 'pk.eyJ1IjoibWFwcGluZ2xlaG5lIiwiYSI6ImNqdmxhZzMyYjEwNXAzeWw2ZTZqcG5waWUifQ.PtU676kvusvROvh_N34FxA'
+  MAPBOX_API_TOKEN: 'pk.eyJ1IjoibWFwcGluZ2xlaG5lIiwiYSI6ImNqdmxhZzMyYjEwNXAzeWw2ZTZqcG5waWUifQ.PtU676kvusvROvh_N34FxA',
+  firebaseConfig: {
+    apiKey: "AIzaSyA1abJCUsKrI58kUPALI3Oz3nLJIgty4ZQ",
+    authDomain: "meine-pwa.firebaseapp.com",
+    databaseURL: "https://meine-pwa.firebaseio.com",
+    projectId: "meine-pwa",
+    storageBucket: "meine-pwa.appspot.com",
+    messagingSenderId: "340118471403",
+    appId: "1:340118471403:web:4e13847887689032"
+  }
 };
 
 /*

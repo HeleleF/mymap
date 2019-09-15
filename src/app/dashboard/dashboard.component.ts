913b802806d2af446e0ff33f42585439597ee1d9
Chris Rebbelin <s0548921@htw-berlin.de>
2019-09-15 18:36:37 +0200

    dashboard badge count added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DbService } from '../shared/db.service';
 import { AuthService } from '../shared/auth.service';
+import { getKeys, GymBadge } from '../model/api.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,15 +12,23 @@ export class DashboardComponent implements OnInit {
 
   badges: number[];
   user: firebase.User;
+  count = 0;
+  readonly badgeNames: string[];
+  loading = true;
 
   constructor(private db: DbService,
               private auth: AuthService) {
+                this.badgeNames = getKeys(GymBadge);
   }
 
   async ngOnInit() {
 
     this.user = await this.auth.getCurrentUser();
     this.badges = await this.db.getBadgeCount();
+
+    this.count = this.badges.reduce((acc, cur) => acc + cur, 0);
+
+    this.loading = false;
   }
 
 }

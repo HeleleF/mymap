dd1292159bb8af5f9098318b5ecea073023b8e17
Chris Rebbelin <s0548921@htw-berlin.de>
2019-09-15 18:35:59 +0200

    added badge filter

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MessageService } from '../shared/message.service';
-import { QuestType, QuestReward, QuestEncounter, FilterSettings, keys, Poke } from '../model/api.model';
+import { QuestType, QuestReward, QuestEncounter, FilterSettings, getKeys, Poke, GymBadge } from '../model/api.model';
 
 import { AutochipsComponent } from '../autochips/autochips.component';
 
@@ -16,25 +16,27 @@ export class FilterComponent {
   readonly types: string[];
   readonly encs: Poke[];
   readonly rewards: string[];
+  readonly badges: string[];
 
   constructor(private popup: MatDialogRef<FilterComponent>,
               private ms: MessageService) {
 
       this.sets = this.ms.getOnce();
-      this.types = keys(QuestType);
-      this.encs = keys(QuestEncounter).map((e, i) => {
+      this.types = getKeys(QuestType);
+      this.encs = getKeys(QuestEncounter).map((e, i) => {
         return {
           dex: i.toString().padStart(3, '0'),
           name: e
         };
       });
-      this.rewards = keys(QuestReward);
+      this.rewards = getKeys(QuestReward);
+      this.badges = getKeys(GymBadge);
   }
 
   changeType(t: string) {
 
     if (this.sets.types.includes(t)) {
-      this.sets.types = this.sets.types.filter(i => i !== t);
+      this.sets.types = this.sets.types.filter((i: string) => i !== t);
     } else {
       this.sets.types.push(t);
     }
@@ -43,9 +45,18 @@ export class FilterComponent {
   changeReward(r: string) {
 
     if (this.sets.rewards.includes(r)) {
-      this.sets.rewards = this.sets.rewards.filter(i => i !== r);
+      this.sets.rewards = this.sets.rewards.filter((i: string) => i !== r);
     } else {
       this.sets.rewards.push(r);
+    }
+  }
+
+  changeBadge(b: number) {
+
+    if (this.sets.badges.includes(b)) {
+      this.sets.badges = this.sets.badges.filter((i: number) => i !== b);
+    } else {
+      this.sets.badges.push(b);
     }
   }
 

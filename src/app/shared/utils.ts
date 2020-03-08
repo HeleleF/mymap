7848f0f64c83a6ddd49cc9e4e5ec1ccc652ae7ef
Chris Rebbelin <s0548921@htw-berlin.de>
2020-03-08 17:07:43 +0100

    Add basic S2 support

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -152,3 +152,224 @@ export function createRows<T>(data: T[], columns = 3): T[][] {
     });
     return data as any;
 }
+
+const d2r = Math.PI / 180;
+const r2d = 1 / d2r;
+
+const LatLngToXYZ = (latLng: { lat: number, lng: number }): XYZ => {
+    const phi = latLng.lat * d2r;
+    const theta = latLng.lng * d2r;
+    const cosphi = Math.cos(phi);
+
+    return [Math.cos(theta) * cosphi, Math.sin(theta) * cosphi, Math.sin(phi)];
+};
+
+const XYZToLatLng = (xyz: XYZ) => {
+    const lat = Math.atan2(xyz[2], Math.sqrt(xyz[0] * xyz[0] + xyz[1] * xyz[1]));
+    const lng = Math.atan2(xyz[1], xyz[0]);
+
+    return { lat: lat * r2d, lng: lng * r2d };
+};
+
+const largestAbsComponent = (xyz: XYZ) => {
+    const temp = [Math.abs(xyz[0]), Math.abs(xyz[1]), Math.abs(xyz[2])];
+
+    if (temp[0] > temp[1]) {
+        if (temp[0] > temp[2]) {
+            return 0;
+        }
+        return 2;
+    }
+
+    if (temp[1] > temp[2]) {
+        return 1;
+    }
+
+    return 2;
+};
+
+const faceXYZToUV = (face: Face, xyz: XYZ): UV => {
+    let u, v;
+
+    switch (face) {
+        case 0: u = xyz[1] / xyz[0]; v = xyz[2] / xyz[0]; break;
+        case 1: u = -xyz[0] / xyz[1]; v = xyz[2] / xyz[1]; break;
+        case 2: u = -xyz[0] / xyz[2]; v = -xyz[1] / xyz[2]; break;
+        case 3: u = xyz[2] / xyz[0]; v = xyz[1] / xyz[0]; break;
+        case 4: u = xyz[2] / xyz[1]; v = -xyz[0] / xyz[1]; break;
+        case 5: u = -xyz[1] / xyz[2]; v = -xyz[0] / xyz[2]; break;
+        default: throw { error: 'Invalid face' };
+    }
+
+    return [u, v];
+};
+
+const XYZToFaceUV = (xyz: XYZ): [ Face, UV ] => {
+    let face = largestAbsComponent(xyz);
+
+    if (xyz[face] < 0) {
+        face += 3;
+    }
+
+    const uv = faceXYZToUV(face as Face, xyz);
+
+    return [face as Face, uv ];
+};
+
+const FaceUVToXYZ = (face: Face, uv: UV): XYZ => {
+    const u = uv[0];
+    const v = uv[1];
+
+    switch (face) {
+        case 0: return [1, u, v];
+        case 1: return [-u, 1, v];
+        case 2: return [-u, -v, 1];
+        case 3: return [-1, -v, -u];
+        case 4: return [v, -1, -u];
+        case 5: return [v, u, -1];
+        default: throw { error: 'Invalid face' };
+    }
+};
+
+const STToUV = (st: ST): UV => {
+    const singleSTtoUV = function (st: number) {
+        if (st >= 0.5) {
+            return (1 / 3.0) * (4 * st * st - 1);
+        }
+        return (1 / 3.0) * (1 - (4 * (1 - st) * (1 - st)));
+
+    };
+
+    return [singleSTtoUV(st[0]), singleSTtoUV(st[1])];
+};
+
+const UVToST = (uv: UV): ST => {
+    const singleUVtoST = function (uv: number) {
+        if (uv >= 0) {
+            return 0.5 * Math.sqrt(1 + 3 * uv);
+        }
+        return 1 - 0.5 * Math.sqrt(1 - 3 * uv);
+
+    };
+
+    return [singleUVtoST(uv[0]), singleUVtoST(uv[1])];
+};
+
+const STToIJ = (st: ST, order: number): IJ => {
+    const maxSize = 1 << order;
+
+    const singleSTtoIJ = function (st: number) {
+        const ij = Math.floor(st * maxSize);
+        return Math.max(0, Math.min(maxSize - 1, ij));
+    };
+
+    return [singleSTtoIJ(st[0]), singleSTtoIJ(st[1])];
+};
+
+const IJToST = (ij: IJ, order: number, offsets: number[]): ST => {
+    const maxSize = 1 << order;
+
+    return [
+        (ij[0] + offsets[0]) / maxSize,
+        (ij[1] + offsets[1]) / maxSize
+    ];
+};
+
+/**
+ * The side of the cube
+ */
+type Face = 0 | 1 | 2 | 3 | 4 | 5;
+type XYZ = [ number, number, number ];
+type UV = [ number, number ];
+type ST = [ number, number ];
+type IJ = [ number, number ];
+
+export class S2Cell {
+
+    private face: Face;
+    private ij: IJ;
+    private level: number;
+
+    constructor(f: Face, ij: IJ, lvl: number) {
+        this.face = f;
+        this.ij = ij;
+        this.level = lvl;
+    }
+
+    /**
+     * Creates a S2Cell from a latlng pair
+     * @param latLng 
+     * @param level 
+     */
+    static fromLatLng(latLng: any, level: number) {
+        const xyz = LatLngToXYZ(latLng);
+        const faceuv = XYZToFaceUV(xyz);
+        const st = UVToST(faceuv[1]);
+        const ij = STToIJ(st, level);
+
+        return new S2Cell(faceuv[0], ij, level);
+    }
+
+    toString() {
+        return `F${this.face}ij[${this.ij[0]}${this.ij[1]}]@${this.level}`;
+    }
+
+    getLatLng(offset: number[] = [0.5, 0.5]) {
+        const st = IJToST(this.ij, this.level, offset);
+        const uv = STToUV(st);
+        const xyz = FaceUVToXYZ(this.face, uv);
+    
+        return XYZToLatLng(xyz);
+    }
+
+    getCornerLatLngs() {
+        return [
+            [0.0, 0.0],
+            [0.0, 1.0],
+            [1.0, 1.0],
+            [1.0, 0.0]
+        ].map(offset => this.getLatLng(offset));
+    }
+
+    getNeighbors() {
+
+        const fromFaceIJWrap = (face: Face, ij: IJ, level: number) => {
+
+            const maxSize = 1 << level;
+
+            if (ij[0] >= 0 && ij[1] >= 0 && ij[0] < maxSize && ij[1] < maxSize) {
+                // no wrapping out of bounds
+                return new S2Cell(face, ij, level);
+            }
+    
+            // the new i,j are out of range.
+            // with the assumption that they're only a little past the borders we can just take the points as
+            // just beyond the cube face, project to XYZ, then re-create FaceUV from the XYZ vector
+            let st = IJToST(ij,level,[0.5, 0.5]);
+            let uv = STToUV(st);
+            let xyz = FaceUVToXYZ(face, uv);
+            const faceuv = XYZToFaceUV(xyz);
+            face = faceuv[0];
+            uv = faceuv[1];
+            st = UVToST(uv);
+            ij = STToIJ(st,level);
+            return new S2Cell(face, ij, level);
+        };
+    
+        const face = this.face;
+        const i = this.ij[0];
+        const j = this.ij[1];
+        const level = this.level;
+
+        return [
+            {a: -1, b: 0},
+            {a: 0, b: -1},
+            {a: 1, b: 0},
+            {a: 0, b: 1}
+        ].map(values => fromFaceIJWrap(face, [i + values.a, j + values.b], level));
+    }
+};
+
+
+
+

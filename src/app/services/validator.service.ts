c254a615fddc2654497c0d521d64c5d93327e7e8
Chris Rebbelin <s0548921@htw-berlin.de>
2020-03-17 17:26:11 +0100

    Rewrite validator

--- src/app/services/validator.service.ts
+++ src/app/services/validator.service.ts
@@ -1,4 +1,4 @@
-import { ValidatorFn, AbstractControl, AsyncValidatorFn } from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -15,36 +15,22 @@ export class ValidatorService {
 
     constructor(private http: HttpClient) {}
 
-    createGymIdValidator(): ValidatorFn {
-
-        const guid = /^[0-9a-f]{32}(\.(1[126]|2))?$/;
-
-        return (control: AbstractControl): { [key: string]: any } | null => {
-            return guid.test(control.value) ? null : { wrongFormat: { value: control.value } };
-        };
+    static validPortalId(control: AbstractControl): ValidationErrors | null {
+        return /^[0-9a-f]{32}(\.(1[126]|2))?$/.test(control.value) ? null : { wrongFormat: { value: control.value } };
     }
 
-    createGymPositionValidator(): ValidatorFn {
-
-        const latlng = /^\d{2}\.\d+\,\d{2}\.\d+$/;
-
-        return (control: AbstractControl): { [key: string]: any } | null => {
-            return latlng.test(control.value) ? null : { malformedPos: { value: control.value } };
-        };
+    static validPosition(control: AbstractControl): ValidationErrors | null {
+        return /^\d{2}\.\d+\,\d{2}\.\d+$/.test(control.value) ? null : { malformedPos: { value: control.value } };
     }
 
-    createGymBadgeValidator(): ValidatorFn {
-
+    static validBadge(control: AbstractControl): ValidationErrors | null {
         const validBadges = getKeys(GymBadge);
-
-        return (control: AbstractControl): { [key: string]: any } | null => {
-            return validBadges.includes(control.value) ? null : { wrongBadge: { value: control.value } };
-        };
+        return validBadges.includes(control.value) ? null : { wrongBadge: { value: control.value } };
     }
-
+    
     createGymUrlValidator(): AsyncValidatorFn {
 
-        return (control: AbstractControl): Observable<{ [key: string]: any } | null> => {
+        return (control: AbstractControl): Observable<ValidationErrors | null> => {
 
             return this.http.get(control.value, { responseType: 'blob' }).pipe(
                 map((blob) => {
@@ -63,7 +49,7 @@ export class ValidatorService {
      */
     parseAndValidate(text: string): {[key: string]: any} {
 
-        const matcher = /description: \"(?<name>.*)\"\s{1,2}url: \"(?<url>.*)\"\s{1,2}location: \"(?<pos>.*)\"\s{1,2}gym_id: \"(?<id>.*)\"\s{1,2}badge: (?<badge>\d)/m;
+        const matcher = /badge: (?<badge>\d)\s{1,2}description: \"(?<name>.*)\"\s{1,2}gym_id: \"(?<id>.*)\"\s{1,2}location: \"(?<pos>.*)\"\s{1,2}url: \"(?<url>.*)\"/m;
 
         const match = matcher.exec(text); // || { groups: { name: null, url: null, pos: null, id: null, badge: null } };
 

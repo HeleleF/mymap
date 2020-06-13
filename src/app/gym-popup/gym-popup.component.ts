78735c09519959774ef166d03e66c1f7a204dc8c
Chris Rebbelin <s0548921@htw-berlin.de>
2020-06-13 16:17:18 +0200

    Create component for gym popups

---
+++ src/app/gym-popup/gym-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gym-popup',
+  templateUrl: './gym-popup.component.html',
+  styleUrls: ['./gym-popup.component.scss']
+})
+export class GymPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

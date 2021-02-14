7e11e0a1b54a1c6f11aae9a562fefe2ac5360868
Chris Rebbelin <s0548921@htw-berlin.de>
2021-02-14 14:56:55 +0100

    Use placeholders instead of rewriting everything

--- scripts/config.index.ts
+++ scripts/config.index.ts
@@ -1,25 +1,12 @@
-import { writeFile } from 'fs';
+import { writeFile, readFileSync } from 'fs';
 
 const targetPath = './src/environments/environment.prod.ts';
 
-//TODO(helene): change this to replace placeholders instead of rewrting everything
-const envConfigFile = `export const environment = {
-   production: true,
-   MAPBOX_STYLE_URI: 'mapbox://styles/mappinglehne/ck4n0h27q0qqe1crzvpg8vpsm',
-   MAPBOX_API_TOKEN: '${process.env.MAPBOX_API_TOKEN}',
-   firebaseConfig: {
-        apiKey: '${process.env.FIREBASE_API_KEY}',
-        authDomain: 'meine-pwa.firebaseapp.com',
-        databaseURL: 'https://meine-pwa.firebaseio.com',
-        projectId: 'meine-pwa',
-        storageBucket: 'meine-pwa.appspot.com',
-        messagingSenderId: '340118471403',
-        appId: '1:340118471403:web:4e13847887689032'
-    }
-};
-`;
+const newEnvConfigFile = readFileSync(targetPath, { encoding: 'utf-8' })
+                            .replace('<PLACEHOLDER_MAPBOX_API_TOKEN>', process.env.MAPBOX_API_TOKEN as string)
+                            .replace('<PLACEHOLDER_FIREBASE_API_KEY>', process.env.FIREBASE_API_KEY as string)
 
-writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+writeFile(targetPath, newEnvConfigFile, 'utf8', (err) => {
 	if (err) {
 		return console.log(err);
 	}

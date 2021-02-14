420f251267e8de900a5427294c44e9e39575d324
Chris Rebbelin <s0548921@htw-berlin.de>
2021-02-14 15:03:27 +0100

    Run prettier

--- scripts/config.index.ts
+++ scripts/config.index.ts
@@ -3,8 +3,14 @@ import { writeFile, readFileSync } from 'fs';
 const targetPath = './src/environments/environment.prod.ts';
 
 const newEnvConfigFile = readFileSync(targetPath, { encoding: 'utf-8' })
-                            .replace('<PLACEHOLDER_MAPBOX_API_TOKEN>', process.env.MAPBOX_API_TOKEN as string)
-                            .replace('<PLACEHOLDER_FIREBASE_API_KEY>', process.env.FIREBASE_API_KEY as string)
+	.replace(
+		'<PLACEHOLDER_MAPBOX_API_TOKEN>',
+		process.env.MAPBOX_API_TOKEN as string
+	)
+	.replace(
+		'<PLACEHOLDER_FIREBASE_API_KEY>',
+		process.env.FIREBASE_API_KEY as string
+	);
 
 writeFile(targetPath, newEnvConfigFile, 'utf8', (err) => {
 	if (err) {
